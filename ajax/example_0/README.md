39cf7f73242be01500338488d2e41d65384ca498
serbipunk <milesbai6022@gmail.com>
2023-08-12 23:37:14 +1200

    header: feature: add an AJAX example

---
+++ ajax/example_0/README.md
@@ -0,0 +1,11 @@
+# AJAX example
+
+## theory
+
+update a website **on the fly** without refresh.
+
+xml is a lacacy name in XMLHttpRequest and AJAX.
+
+after load the json, you can render the page component with EventListener.
+
+reference video: https://youtu.be/rJesac0_Ftw
---
+++ ajax/example_0/main.js
@@ -0,0 +1,30 @@
+var pageCounter = 1;
+var animalContainer = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function() {  // anonymous function
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-1.json");
+
+    ourRequest.onload = function() {
+        // console.log(ourRequest.responseText);
+        // var ourData = ourRequest.responseText;
+        var ourData = ourRequest.responseText;
+        ourData = JSON.parse(ourData);
+        // console.log(ourData[0]);
+        renderHTML(ourData);
+    };
+    ourRequest.send();  // send the request
+    pageCounter++;
+    if(pageCounter > 3) {
+        // btn.classList.add("hide-me");
+        btn.style.display = "none";
+    }
+});
+
+function renderHTML(data) {
+    var htmlString = "";
+    for(var i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+    }
+    animalContainer.insertAdjacentHTML("beforeend", htmlString); 
+}
